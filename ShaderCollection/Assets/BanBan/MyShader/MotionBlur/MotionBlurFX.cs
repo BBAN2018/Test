d3587b8e100688c71501ee922245d61dfd340dbf
boki2016 <asd3333fg@gmail.com>
2018-08-20 01:14:55 +0900

    no message

---
+++ ShaderCollection/Assets/BanBan/MyShader/MotionBlur/MotionBlurFX.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class MotionBlurFX : MonoBehaviour {
+
+	public Shader blurShader = null;	
+
+	public Material m_Material = null;
+	protected Material material {
+		get {
+			if (m_Material == null) {
+				m_Material = new Material(blurShader);
+				m_Material.hideFlags = HideFlags.DontSave;
+			}
+			return m_Material;
+		} 
+	}
+	
+	protected void OnDisable() {
+		if( m_Material ) {
+			DestroyImmediate( m_Material );
+		}
+	}	
+	
+	// --------------------------------------------------------
+	
+	protected void Start()
+	{
+		// Disable if we don't support image effects
+		if (!SystemInfo.supportsImageEffects) {
+			enabled = false;
+			return;
+		}
+		// Disable if the shader can't run on the users graphics card
+		if (!blurShader || !material.shader.isSupported) {
+			enabled = false;
+			return;
+		}
+	}
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination) 
+	{		
+		if(!blurShader)
+			return;
+
+		RenderTexture BufferTex = source;
+
+		Graphics.Blit(source, destination,material);
+	}	
+}
---
+++ ShaderCollection/Assets/BanBan/MyShader/MotionBlur/MotionBlurFX.shader
@@ -0,0 +1,47 @@
+﻿Shader "Ban/MotionBlurFX" 
+{
+	Properties 
+	{
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("MainTex", 2D) = "white" {}
+		_MaskAmount("MaskAmount" , Range(0,1)) = 0
+	}
+	SubShader 
+	{
+		Tags {"Queue" = "Transparent" "RenderType"="Transparent" }
+		LOD 300
+
+		Cull Off
+		Zwrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+		Pass
+		{
+		CGPROGRAM
+	
+		#pragma vertex vert_img
+		#pragma fragment frag
+		#include "UnityCG.cginc"
+
+		float _MaskAmount;
+		fixed4 _Color;
+		sampler2D _MainTex;
+
+		fixed4 frag(v2f_img i) : COLOR
+		{
+			fixed4 Col = tex2D(_MainTex,i.uv);
+			fixed4 FinalCol = Col * _Color;
+
+			if((FinalCol.r + FinalCol.g + FinalCol.b)/3 < _MaskAmount)
+			{
+				return fixed4(1,0,0,1);
+				discard;
+			}
+
+			return FinalCol;
+		}
+
+		ENDCG
+	}
+	}
+	FallBack "Diffuse"
+}
